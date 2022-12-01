3f5bb133c1686f809d96307b2e62ac17954981dc
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-01 10:06:50 +0200

    class component review include search function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import "./App.css";
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
@@ -12,7 +13,6 @@ class App extends Component {
     };
   }
   componentDidMount() {
-    console.log("2");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -38,21 +38,12 @@ class App extends Component {
     });
     return (
       <div className="App">
-        {
-          <input
-            className="search-box"
-            type="search"
-            placeholder="search monsters"
-            onChange={onSearchChange}
-          />
-        }
-        {/*  {filteredMonsters.map((monster) => {
-          return (
-            <div key={monster.name}>
-              <p>{monster.name}</p>
-            </div>
-          );
-        })} */}
+        <SearchBox
+          className="search-box"
+          placeholder="Search monsters"
+          onChangeHandler={onSearchChange}
+        />
+
         <CardList monsters={filteredMonsters} />
       </div>
     );
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,16 +1,21 @@
 import {Component} from 'react';
+import Card from '../card/card.component';
+import './card-list.styles.css';
+
 
 class CardList extends Component {
     render() {
+        console.log(this.props.monsters)
+        console.log("render")
         const {monsters} = this.props;
 
         return(
-            <div>
+            <div className='card-list'>
                 {monsters.map((monster) => {
+                    // destructure where ever variable is repeating
+                   
                  return (
-                    <div key={monster.name}>
-                        <p>{monster.name}</p>
-                    </div>
+                    <Card monster={monster}/>
                     )
                 })}
             </div>
