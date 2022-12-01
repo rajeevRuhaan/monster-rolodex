145c47683cabce07186ad23b798e80e13a054d5a
rajeevRuhaan <urrajivin@hotmail.com>
2022-12-01 07:07:39 +0200

    card-list.component added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import CardList from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
-      name: "Ruhaan",
+      monsters: [],
+      searchField: "",
     };
   }
+  componentDidMount() {
+    console.log("2");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (e) => {
+    const searchField = e.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+    const { onSearchChange } = this;
+    const { monsters, searchField } = this.state;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button>Change Name</button>
-        </header>
+        {
+          <input
+            className="search-box"
+            type="search"
+            placeholder="search monsters"
+            onChange={onSearchChange}
+          />
+        }
+        {/*  {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.name}>
+              <p>{monster.name}</p>
+            </div>
+          );
+        })} */}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
